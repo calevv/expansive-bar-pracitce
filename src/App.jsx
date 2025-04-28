c8b130621694248996a8001afe05ee42e481d4eb
calevv <ika818488@gmail.com>
2025-04-28 14:02:05 +0900

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import AppBar from "./components/AppBar/AppBar";
-
+import { useRef } from "react";
 function App() {
   const [text, setText] = useState("");
+  const inputRef = useRef(null);
   const [message, setMessage] = useState("");
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
   const submitText = (e) => {
+    if (text === "") {
+      inputRef.current.focus();
+    }
     e.preventDefault();
     setMessage(text);
   };
@@ -17,7 +21,12 @@ function App() {
       <AppBar />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <form onSubmit={submitText}>
-          <input type="text" value={text} onChange={handleTextChange} />
+          <input
+            type="text"
+            value={text}
+            onChange={handleTextChange}
+            ref={inputRef}
+          />
           <button type="submit">send</button>
         </form>
       </div>
